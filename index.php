bd56a637db876d0079489ad2a058b04ef4f38feb
Wioleta Sztejter <wiola0406@gmail.com>
2023-04-19 19:08:51 +0200

    Add exception

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 declare(strict_types=1);
 
 namespace App;
+
+require_once('./exception/appException.php');
+include_once('./src/controller.php');
 include_once ('./src/utils/debug.php');
-include_once('./src/Controller.php');
 require_once('./config/config.php');
 
+try{
 controller::initConfiguration($configuration);
 $controller = new Controller($_GET, $_POST);
-$controller->run();
+$controller->run();}
+catch(AppException $e){
+echo "<h1>Wystąpił błąd w aplikacji</h1>";
+echo '<h3>' .$e->getMessage() . '</h3>';
+}
