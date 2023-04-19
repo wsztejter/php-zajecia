5e0d0a5f5a04b63ac129498929930dd35fac1fac
Wioleta Sztejter <wiola0406@gmail.com>
2023-04-19 19:25:16 +0200

    Add Exception

--- src/database.php
+++ src/database.php
@@ -3,11 +3,17 @@ declare(strict_types=1);
 
 namespace App;
 
+use Exception;
 use PDO;
+use PDOException;
 
 class database{
     public function __construct(array $config){
+        try { 
         $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
-        $connection = new PDO('sssss');
+        $connection = new PDO('sssss');}
+        catch (PDOException $e){
+            throw new AppException ('Błąd w połączeniu z bazą danych');
+        }
     }
 }
